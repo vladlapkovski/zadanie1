c31d8886000540a5331b127af16a2f35856335f6
vladlapkovski <vlad.lapkofffskii@gmail.com>
2023-09-25 17:39:02 +0300

    dist into ignore

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,28 @@
-import express, {Request, Response} from "express"
+import express, { Request, Response } from "express";
 
-const app = express()
+const app = express();
 
-let resolution = ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]
+enum Resolution {
+  P144 = "P144",
+  P240 = "P240",
+  P360 = "P360",
+  P480 = "P480",
+  P720 = "P720",
+  P1080 = "P1080",
+  P1440 = "P1440",
+  P2160 = "P2160",
+}
+
+let resolution: Resolution[] = [
+  Resolution.P144,
+  Resolution.P240,
+  Resolution.P360,
+  Resolution.P480,
+  Resolution.P720,
+  Resolution.P1080,
+  Resolution.P1440,
+  Resolution.P2160,
+];
 
 interface Videos {
   id: number;
@@ -12,184 +32,160 @@ interface Videos {
   minAgeRestriction: number | null;
   createdAt: string;
   publicationDate: string;
-  availableResolutions: any[];
+  availableResolutions: Resolution[];
 }
 
 const videos: Videos[] = [
-  // {
-  //   id: 1, 
-  //   title: "nigth show", 
-  //   author: "dimych", 
-  //   canBeDownloaded: true, 
-  //   minAgeRestriction: null,
-  //   createdAt: new Date().toISOString(), 
-  //   publicationDate: new Date(Date.now() + 86400000).toISOString(), 
-  //   availableResolutions: ["P144"]
-  // }, 
+  {
+    id: 1,
+    title: "nigth show",
+    author: "dimych",
+    canBeDownloaded: true,
+    minAgeRestriction: null,
+    createdAt: new Date().toISOString(),
+    publicationDate: new Date(Date.now() + 86400000).toISOString(),
+    availableResolutions: [Resolution.P144],
+  },
+];
 
-  // {
-  //   id: 2, 
-  //   title: "kuchna", 
-  //   author: "kto-to", 
-  //   canBeDownloaded: true, 
-  //   minAgeRestriction: null, 
-  //   createdAt: new Date().toISOString(), 
-  //   publicationDate: new Date(Date.now() + 86400000).toISOString(), 
-  //   availableResolutions: ["P144"]
-  // }
-]
-
-
-const parserMiddleware = express.json()
-app.use(parserMiddleware)
-
+const parserMiddleware = express.json();
+app.use(parserMiddleware);
 
 app.get("/hometask_01/api/videos", (req: Request, res: Response) => {
-  res.status(200).send(videos)
-  return
-})
-
-app.get("/hometask_01/api/videos/:videoId", (req: Request, res: Response) => {
-    let video = videos.find(v => v.id === +req.params.videoId)
-
-    if (!video) {
-      return res.status(404).send()
-    }
-    
-    return res.status(200).send(video)
-})
-
-app.delete("/hometask_01/api/videos/:videoId", (req: Request, res: Response) => {
-    for (let i = 0; i < videos.length; i++) {
-       if (videos[i].id === +req.params.videoId) {
-          videos.splice(i, 1);
-          res.status(204).send();
-          return;
-       }
-    } 
-
-    return  res.status(404).send()
-})
-
-app.delete("/testing/all-data", (req: Request, res: Response) => {     
-    videos.length = 0;
-    return res.status(204).send();        
-})
-
-app.post('/hometask_01/api/videos', (req: Request, res: Response) => {
-  interface CreateVideoInputModel {
-    title: string;
-    author: string;
-    availableResolutions?: string[];
-    canBeDownloaded?: boolean;
-    minAgeRestriction?: number | null;
-    createdAt?: string;
-    publicationDate?: string;
-  }
-
-  const { title, author, availableResolutions, canBeDownloaded = false, minAgeRestriction = null, createdAt = new Date().toISOString(), publicationDate = new Date(Date.now() + 86400000).toISOString() } = req.body as CreateVideoInputModel;
-
-  // Check for required fields
-  if (!title || !author) {
-    res.status(400).json({
-      errorsMessages: [
-        {
-          message: "Title and author are required",
-          field: !title ? "title" : "author"
-        }
-      ]
-    });
-    return;
-  }
-
-  // Check for max length
-  if (title.length > 40 || author.length > 20) {
-    res.status(400).json({
-      errorsMessages: [
-        {
-          message: "Title or author is too long",
-          field: title.length > 40 ? "title" : "author"
-        }
-      ]
-    });
-    return;
-  }
-
-  // Check for available resolutions
-  if (!availableResolutions || availableResolutions.length === 0) {
-    res.status(400).json({
-      errorsMessages: [
-        {
-          message: "At least one resolution should be added",
-          field: "availableResolutions"
-        }
-      ]
-    });
-    return;
-  }
-
-  // Check for valid resolutions
- const invalidResolutions = availableResolutions.filter(resolution => !resolution || !resolution.includes(resolution));
-  if (invalidResolutions.length > 0) {
-    res.status(400).json({
-      errorsMessages: invalidResolutions.map(resolution => ({
-        message: "Invalid resolution",
-        field: "availableResolutions"
-      }))
-    });
-    return;
-  }
-
-  // Add new video
-  const newVideo: Videos = {
-    id: videos.length + 1,
-    title,
-    author,
-    canBeDownloaded,
-    minAgeRestriction,
-    createdAt,
-    publicationDate,
-    availableResolutions
-  };
-  videos.push(newVideo);
-
-  return res.status(201).json({ video: newVideo });
+  res.status(200).send(videos);
 });
 
+app.get("/hometask_01/api/videos/:videoId", (req: Request, res: Response) => {
+  let video = videos.find((v) => v.id === +req.params.videoId);
 
-
-app.put("/hometask_01/api/videos/:videoId", (req: Request, res: Response) => {
-  let video = videos.find(v => v.id === +req.params.videoId)
-
-  if (req.body.title.length > 40 ||
-     req.body.author.length > 20 || 
-     req.body.availableResolutions.length < 1 || 
-     !req.body.availableResolutions.every((r: string) => resolution.includes(r)) || 
-     typeof(req.body.canBeDownloaded) !== "boolean" || 
-     req.body.minAgeRestriction < 1 || req.body.minAgeRestriction > 18) {
-    res.status(400).send()
-    return
-  } 
-
-  if(!video){
-    res.status(404).send()
-      return
+  if (!video) {
+    return res.status(404).send();
   }
 
+  return res.status(200).send(video);
+});
 
-  video.title = req.body.title,
-  video.author = req.body.author,
-  video.availableResolutions = req.body.availableResolutions,
-  video.canBeDownloaded = req.body.canBeDownloaded,
-  video.minAgeRestriction = req.body.minAgeRestriction,
-  video.publicationDate = new Date(Date.now() + 86400000).toISOString()
-  
-  return res.status(204).send(video)
-})
+app.delete("/hometask_01/api/videos/:videoId", (req: Request, res: Response) => {
+  for (let i = 0; i < videos.length; i++) {
+    if (videos[i].id === +req.params.videoId) {
+      videos.splice(i, 1);
+      res.status(204).send();
+      return;
+    }
+  }
 
-const port = process.env.PORT || 3000
+  return res.status(404).send();
+});
 
+app.delete("/testing/all-data", (req: Request, res: Response) => {
+  videos.length = 0;
+  return res.status(204).send();
+});
 
-app.listen(port, () => {
-    console.log(`app listening on port: ${port}`)
-})
+app.put("/hometask_01/api/videos/:videoId", (req: Request, res: Response) => {
+  let video = videos.find((v) => v.id === +req.params.videoId);
+
+  if (!video) {
+    return res.status(404).send();
+  }
+
+  if (
+    !req.body.title ||
+    typeof req.body.title !== "string" ||
+    req.body.title.length > 40
+  ) {
+    return res.status(400).send("Invalid title");
+  }
+
+  if (
+    !req.body.author ||
+    typeof req.body.author !== "string" ||
+    req.body.author.length > 20
+  ) {
+    return res.status(400).send("Invalid author");
+  }
+
+  if (
+    !req.body.availableResolutions ||
+    !Array.isArray(req.body.availableResolutions) ||
+    req.body.availableResolutions.length < 1 ||
+    !req.body.availableResolutions.every((r: Resolution) =>
+      resolution.includes(r)
+    )
+  ) {
+    return res.status(400).send("Invalid available resolutions");
+  }
+
+  if (typeof req.body.canBeDownloaded !== "boolean") {
+    return res.status(400).send("Invalid canBeDownloaded");
+  }
+
+  if (
+    req.body.minAgeRestriction !== null &&
+    (typeof req.body.minAgeRestriction !== "number" ||
+      req.body.minAgeRestriction < 1 ||
+      req.body.minAgeRestriction > 18)
+  ) {
+    return res.status(400).send("Invalid minAgeRestriction");
+  }
+
+  video.title = req.body.title;
+  video.author = req.body.author;
+  video.availableResolutions = req.body.availableResolutions;
+  video.canBeDownloaded = req.body.canBeDownloaded;
+  video.minAgeRestriction = req.body.minAgeRestriction;
+  video.publicationDate = new Date(Date.now() + 86400000).toISOString();
+
+  return res.status(204).send(video);
+});
+
+app.post("/hometask_01/api/videos", (req: Request, res: Response) => {
+  if (
+    !req.body.title ||
+    typeof req.body.title !== "string" ||
+    req.body.title.length > 40
+  ) {
+    return res.status(400).send("Invalid title");
+  }
+
+  if (
+    !req.body.author ||
+    typeof req.body.author !== "string" ||
+    req.body.author.length > 20
+  ) {
+    return res.status(400).send("Invalid author");
+  }
+
+  if (
+    !req.body.availableResolutions ||
+    !Array.isArray(req.body.availableResolutions) ||
+    req.body.availableResolutions.length < 1 ||
+    !req.body.availableResolutions.every((r: Resolution) =>
+      resolution.includes(r)
+    )
+  ) {return res.status(400).send("Invalid available resolutions");
+}
+
+if (typeof req.body.canBeDownloaded !== "boolean") {
+  return res.status(400).send("Invalid canBeDownloaded");
+}
+
+const newVideo: Videos = {
+  id: videos.length + 1,
+  title: req.body.title,
+  author: req.body.author,
+  canBeDownloaded: req.body.canBeDownloaded,
+  minAgeRestriction: req.body.minAgeRestriction,
+  createdAt: new Date().toISOString(),
+  publicationDate: new Date(Date.now() + 86400000).toISOString(),
+  availableResolutions: req.body.availableResolutions,
+};
+
+videos.push(newVideo);
+
+return res.status(201).send(newVideo);
+});
+
+app.listen(3000, () => {
+console.log("Server is listening on port 3000");
+});
